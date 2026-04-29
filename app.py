858d42bc13502f9b277198679ea31119cc63ff63
Zainab <zainabmasood@gmail.com>
2026-04-29 06:52:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,8 @@ def home():
     return f"Secret is: {os.getenv('SECRET_KEY', 'Not Set')}"
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
